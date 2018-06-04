e7ed781615c10703b9ad73258872e2bff2366d8d
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2018-06-04 15:37:26 -0300

    quase B

--- pic.py
+++ pic.py
@@ -1,23 +1,35 @@
 import pybullet as p
 import time
 from random import randint
+from script import escreve_urdf
 
 p.connect(p.GUI)
-
-p.resetSimulation()
-p.setGravity(0,0,-10)
-
-p.createCollisionShape(p.GEOM_PLANE)
-p.createMultiBody(0,0)
-
-obj = p.loadURDF("nicnic.urdf")
-
 p.setRealTimeSimulation(1)
-while(1):
-	for i in range(p.getNumJoints(obj)):
-		maxVel = randint(50,200)
-		maxForce = randint(10, 500)
-		p.setJointMotorControl2(obj, i, p.VELOCITY_CONTROL,targetVelocity=20,force=500)
-	p.stepSimulation()
-	time.sleep(0.01)
+
+
+for i in range(5):
+	escreve_urdf()
+	print("-----------------------------------------------------------------------------------------------------------")
+	p.resetSimulation()
+	p.setGravity(0,0,-10)
+
+	p.createCollisionShape(p.GEOM_PLANE)
+	p.createMultiBody(0,0)
+
+	obj = p.loadURDF("oba.urdf")
+	dist = 0
+	while(dist < 5):
+
+		for j in range(p.getNumJoints(obj)):
+			maxVel = randint(5,30)
+			maxForce = randint(10, 500)
+			p.setJointMotorControl2(obj, j, p.VELOCITY_CONTROL,targetVelocity=maxVel,force=maxForce)
+			
+
+			pos = p.getLinkState(obj,j)[0]
+			dist = (pos[0]**2 + pos[1]**2)**0.5
+			if dist >= 5:
+				break
+		p.stepSimulation()
+		time.sleep(0.01)
 p.disconnect()
