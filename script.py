a297456d170b5bcf4ed3e75f456386b4ac0c0ed6
LiuSeeker <vitor.liu@gmail.com>
2018-06-04 15:32:21 -0300

    melhoria script

--- script.py
+++ script.py
@@ -13,14 +13,80 @@ class Link:
 		self.peca_my = True
 		self.peca_z = True
 		self.peca_mz = True
+		self.list_dist = 0
 
-def cria_carro():
+	def dist(self, size_p, size_t):
+		dist_origens = []
+		for h in range(len(size_p)):
+			if len(size_t) == len(size_p):
+				if len(size_t) == 3:
+					dist_origens.append(size_p[h]/2+size_t[h]/2)
+				else:
+					dist_origens.append(size_p[h]+size_t[h])
+
+			elif len(size_t) > len(size_p) and len(size_p) == 1:
+				for j in range(len(size_t)):
+					if len(size_t) == 3:
+						dist_origens.append(size_t[h]/2 +size_p[0])
+					else:
+						dist_origens.append(size_t[h]+size_p[0])
+
+
+			elif len(size_t) > len(size_p) and len(size_p) == 2:
+				if h == 0:
+					if len(size_t) == 3:
+						dist_origens.append(size_t[0]/2+size_p[h])
+						dist_origens.append(size_t[1]/2+size_p[h])
+					else:
+						dist_origens.append(size_t[0]+size_p[h])
+						dist_origens.append(size_t[1]+size_p[h])
+				else:
+					if len(size_t) == 3:
+						dist_origens.append(size_t[2]/2+size_p[h])
+					else:
+						dist_origens.append(size_t[2]+size_p[h])
+
+
+			elif len(size_t) < len(size_p) and len(size_t) == 1:
+				if len(size_t) == 3:
+					dist_origens.append(size_t[0]/2+size_p[h])
+				else:
+					dist_origens.append(size_t[0]+size_p[h])
+
+
+			elif len(size_t) < len(size_p) and len(size_t) == 2:
+				if h <= 1:
+					if len(size_t) == 3:
+						dist_origens.append(size_t[0]/2+size_p[h])
+					else:
+						dist_origens.append(size_t[0]+size_p[h])
+				else:
+					if len(size_t) == 3:
+						dist_origens.append(size_t[1]/2+size_p[h])
+					else:
+						dist_origens.append(size_t[1]+size_p[h])
+
+		self.list_dist = dist_origens
+
+class Joint:
+	def __init__(self, nome, tipo, parent, child):
+		self.nome = nome
+		self.tipo = tipo
+		self.parent = parent
+		self.child = child
+		self.vel = 0
+		self.force = 0
+
+
+def cria_urdf():
 	global dic_links
+	global dic_joints
+	dic_joints = {}
 	#n_pneus = randint(3,6)
-	#n_pecas = randint(2,5)
-	n_pecas = 4
+	n_pecas = randint(2,5)
+	#n_pecas = 3
 
-	shapes = ["box", "cylinder", "sphere"]
+	shapes = ["box", "cylinder", "sphere"] # Tipos de shapes do link
 
 	root = Element("robot", {'name': 'nome_robo'})
 
@@ -30,13 +96,14 @@ def cria_carro():
 	mass_p = SubElement(inertial_p, "mass", {'value': '5'})
 	inertia_p = SubElement(inertial_p, "inertia", {'ixx': '1.0', 'ixy': '0.0', 'ixz': '0.0', 'iyy': '1.0', 'iyz': '0.0','izz': '1.0'})
 
-	randshape = shapes[randint(0,2)]
+	randshape = shapes[randint(0,2)] # Escolhe shape
 
 	collision_p = SubElement(link_principal, "collision")
-	origin_p_c = SubElement(collision_p, "origin", {'xyz': '0 0 0'})
+	origin_p_c = SubElement(collision_p, "origin", {'xyz': '0 0 1'})
 	geometry_p_c = SubElement(collision_p, "geometry")
 
 
+	# Define as dimensões baseadas no shape
 	if randshape == "box":
 		size_list = [round(uniform(0.3,1.5),1), round(uniform(0.3,1.5),1), round(uniform(0.3,1.5),1)]
 		size_list_str = []
@@ -56,11 +123,12 @@ def cria_carro():
 			size_list_str.append(str(size))
 		shape_p_c = SubElement(geometry_p_c, randshape, {'radius': ''.join(size_list_str)})
 
+	# Lista das dimensões do link
 	sizes_link_p = size_list
 	sizes_link_str_p = size_list_str
 
 	visual_p = SubElement(link_principal, "visual")
-	origin_p_v = SubElement(visual_p, "origin", {'xyz': '0 0 0'})
+	origin_p_v = SubElement(visual_p, "origin", {'xyz': '0 0 1'})
 	geometry_p_v = SubElement(visual_p, "geometry")
 
 	if randshape == "box":
@@ -70,9 +138,11 @@ def cria_carro():
 	if randshape == "sphere":
 		shape_p_v = SubElement(geometry_p_v, randshape, {'radius': ''.join(size_list_str)})
 
+	# Adiciona o link ao dicionario
 	dic_links = {"link_principal": Link("link_principal", randshape, sizes_link_p)}
 
 	########
+
 	for i in range(n_pecas-1):
 		nome_link = 'link' + str(i+1)
 		link = SubElement(root, "link", {'name': nome_link})
@@ -81,12 +151,13 @@ def cria_carro():
 		mass_l = SubElement(inertial_l, "mass", {'value': '5'})
 		inertia_l = SubElement(inertial_l, "inertia", {'ixx': '1.0', 'ixy': '0.0', 'ixz': '0.0', 'iyy': '1.0', 'iyz': '0.0','izz': '1.0'})
 
-		randshape = shapes[randint(0,2)]
+		randshape = shapes[randint(0,2)] # Escolhe shape
 
 		collision_l = SubElement(link, "collision")
-		origin_l_c = SubElement(collision_l, "origin", {'xyz': '0 0 0'})
+		origin_l_c = SubElement(collision_l, "origin", {'xyz': '0 0 1'})
 		geometry_l_c = SubElement(collision_l, "geometry")
 
+		# Define as dimensões baseadas no shape
 		if randshape == "box":
 			size_list = [round(uniform(0.3,1.5),1), round(uniform(0.3,1.5),1), round(uniform(0.3,1.5),1)]
 			size_list_str = []
@@ -106,11 +177,12 @@ def cria_carro():
 				size_list_str.append(str(size))
 			shape_l_c = SubElement(geometry_l_c, randshape, {'radius': ''.join(size_list_str)})
 
+		# Lista das dimensões do link
 		sizes_link_t = size_list
 		sizes_link_str_t = size_list_str
 
 		visual_l = SubElement(link, "visual")
-		origin_l_v = SubElement(visual_l, "origin", {'xyz': '0 0 0'})
+		origin_l_v = SubElement(visual_l, "origin", {'xyz': '0 0 1'})
 		geometry_l_v = SubElement(visual_l, "geometry")
 
 		if randshape == "box":
@@ -120,6 +192,7 @@ def cria_carro():
 		elif randshape == "sphere":
 			shape_l_v = SubElement(geometry_l_v, randshape, {'radius': ''.join(size_list_str)})
 
+		# Adiciona o link ao dicionario
 		dic_link_t = {i: Link(nome_link, randshape, sizes_link_t)}
 		dic_links.update(dic_link_t)
 
@@ -139,126 +212,96 @@ def cria_carro():
 		link_parente = dic_links[rand_key].nome
 		list_sizes = dic_links[rand_key].dimensoes
 
-		print(list_sizes, sizes_link_t)
 
-		dist_origens = []
-		for h in range(len(list_sizes)):
-			if len(sizes_link_t) == len(list_sizes):
-				if len(sizes_link_t) == 3:
-					dist_origens.append(list_sizes[h]/2+sizes_link_t[h]/2)
-				else:
-					dist_origens.append(list_sizes[h]+sizes_link_t[h])
-
-			elif len(sizes_link_t) > len(list_sizes) and len(list_sizes) == 1:
-				for j in range(len(sizes_link_t)):
-					if len(sizes_link_t) == 3:
-						dist_origens.append(sizes_link_t[h]/2 +list_sizes[0])
-					else:
-						dist_origens.append(sizes_link_t[h]+list_sizes[0])
-
-
-			elif len(sizes_link_t) > len(list_sizes) and len(list_sizes) == 2:
-				if h == 0:
-					if len(sizes_link_t) == 3:
-						dist_origens.append(sizes_link_t[0]/2+list_sizes[h])
-						dist_origens.append(sizes_link_t[1]/2+list_sizes[h])
-					else:
-						dist_origens.append(sizes_link_t[0]+list_sizes[h])
-						dist_origens.append(sizes_link_t[1]+list_sizes[h])
-				else:
-					if len(sizes_link_t) == 3:
-						dist_origens.append(sizes_link_t[2]/2+list_sizes[h])
-					else:
-						dist_origens.append(sizes_link_t[2]+list_sizes[h])
-
-
-			elif len(sizes_link_t) < len(list_sizes) and len(sizes_link_t) == 1:
-				if len(sizes_link_t) == 3:
-					dist_origens.append(sizes_link_t[0]/2+list_sizes[h])
-				else:
-					dist_origens.append(sizes_link_t[0]+list_sizes[h])
-
-
-			elif len(sizes_link_t) < len(list_sizes) and len(sizes_link_t) == 2:
-				if h <= 1:
-					if len(sizes_link_t) == 3:
-						dist_origens.append(sizes_link_t[0]/2+list_sizes[h])
-					else:
-						dist_origens.append(sizes_link_t[0]+list_sizes[h])
-				else:
-					if len(sizes_link_t) == 3:
-						dist_origens.append(sizes_link_t[1]/2+list_sizes[h])
-					else:
-						dist_origens.append(sizes_link_t[1]+list_sizes[h])
+		dic_links[i].dist(list_sizes, sizes_link_t)
+		
 
 		loop_lado = True
 		while loop_lado:
-			rand_lado = randint(0, len(list_sizes)-1)
+			rand_lado = randint(0,2)
+
 			rand_lado2 = randint(0,1)
 
 			if rand_lado == 0:
-				rand_dist = round(uniform(list_sizes[0],dist_origens[0]),1)
+				rand_dist = round(uniform(list_sizes[0],dic_links[i].list_dist[0]),1)
 				if rand_lado2 == 0 and dic_links[rand_key].peca_x:
-					size_list_j = [str(rand_dist), '0', '0']
+					size_list_j = [str(rand_dist), '0', '1']
 					dic_links[rand_key].peca_x = False
 					dic_links[i].peca_mx = False
 					loop_lado = False
 
 				elif rand_lado2 == 1 and dic_links[rand_key].peca_mx:
-					size_list_j = ["-"+str(rand_dist), '0', '0']
+					size_list_j = ["-"+str(rand_dist), '0', '1']
 					dic_links[rand_key].peca_mx = False
 					dic_links[i].peca_x = False
 					loop_lado = False
 
 			if rand_lado == 1:
-				if len(dist_origens) == 1:
-					rand_dist = round(uniform(list_sizes[1],dist_origens[0]),1)
+				if len(dic_links[i].list_dist) == 1 and len(list_sizes) == 1:
+					rand_dist = round(uniform(list_sizes[0],dic_links[i].list_dist[0]),1)
+				elif len(dic_links[i].list_dist) == 1:
+					rand_dist = round(uniform(list_sizes[1],dic_links[i].list_dist[0]),1)
+				elif len(list_sizes) == 1:
+					rand_dist = round(uniform(list_sizes[0],dic_links[i].list_dist[1]),1)
 				else:
-					rand_dist = round(uniform(list_sizes[1],dist_origens[1]),1)
+					rand_dist = round(uniform(list_sizes[1],dic_links[i].list_dist[1]),1)
 				if rand_lado2 == 0 and dic_links[rand_key].peca_y:
-					size_list_j = ['0', str(rand_dist), '0']
+					size_list_j = ['0', str(rand_dist), '1']
 					dic_links[rand_key].peca_y = False
 					dic_links[i].peca_my = False
 					loop_lado = False
 
 				elif rand_lado2 == 1 and dic_links[rand_key].peca_my:
-					size_list_j = ['0', "-"+str(rand_dist), '0']
+					size_list_j = ['0', "-"+str(rand_dist), '1']
 					dic_links[rand_key].peca_my = False
 					dic_links[i].peca_y = False
 					loop_lado = False
 
 			if rand_lado == 2:
-				if len(dist_origens) == 2:
-					rand_dist = round(uniform(list_sizes[2],dist_origens[1]),1)
-				elif len(dist_origens) == 1:
-					rand_dist = round(uniform(list_sizes[2],dist_origens[0]),1)
+				if len(dic_links[i].list_dist) == 1 and len(list_sizes) == 1:
+					rand_dist = round(uniform(list_sizes[0],dic_links[i].list_dist[0]),1)
+				elif len(dic_links[i].list_dist) == 2 and len(list_sizes) == 1:
+					rand_dist = round(uniform(list_sizes[0],dic_links[i].list_dist[1]),1)
+				elif len(dic_links[i].list_dist) == 3 and len(list_sizes) == 1:
+					rand_dist = round(uniform(list_sizes[0],dic_links[i].list_dist[2]),1)
+				elif len(dic_links[i].list_dist) == 1 and len(list_sizes) == 2:
+					rand_dist = round(uniform(list_sizes[1],dic_links[i].list_dist[0]),1)
+				elif len(dic_links[i].list_dist) == 2 and len(list_sizes) == 2:
+					rand_dist = round(uniform(list_sizes[1],dic_links[i].list_dist[1]),1)
+				elif len(dic_links[i].list_dist) == 3 and len(list_sizes) == 2:
+					rand_dist = round(uniform(list_sizes[1],dic_links[i].list_dist[2]),1)
+				elif len(dic_links[i].list_dist) == 2 and len(list_sizes) == 3:
+					rand_dist = round(uniform(list_sizes[2],dic_links[i].list_dist[1]),1)
+				elif len(dic_links[i].list_dist) == 1 and len(list_sizes) == 3:
+					rand_dist = round(uniform(list_sizes[2],dic_links[i].list_dist[0]),1)
 				else:
-					rand_dist = round(uniform(list_sizes[2],dist_origens[2]),1)
+					rand_dist = round(uniform(list_sizes[2],dic_links[i].list_dist[2]),1)
 
 				if rand_lado2 == 0 and dic_links[rand_key].peca_z:
-					size_list_j = ['0', '0', str(rand_dist)]
+					size_list_j = ['0', '0', str(rand_dist+1)]
 					dic_links[rand_key].peca_z = False
 					dic_links[i].peca_mz = False
 					loop_lado = False
 
-				elif rand_lado2 == 1 and dic_links[rand_key].peca_mz:
-					size_list_j = ['0', '0', "-"+str(rand_dist)]
-					dic_links[rand_key].peca_mz = False
-					dic_links[i].peca_z = False
-					loop_lado = False
-
 		joint_types = ["fixed", "continuous", "continuous"]
 
-		joint = SubElement(root, "joint", {'name': nome_joint, 'type': choice(joint_types)})
+		rand_type = choice(joint_types)
+
+		joint = SubElement(root, "joint", {'name': nome_joint, 'type': rand_type})
 		origin_j = SubElement(joint, "origin", {'xyz': ' '.join(size_list_j)})
 		parent_j = SubElement(joint, "parent", {'link': link_parente})
 		parent_j = SubElement(joint, "child", {'link': nome_link})
+
+		dic_joint_t = {i: Joint(nome_joint, rand_type, link_parente, nome_link)}
+		dic_joints.update(dic_joint_t)
 
 		###################
 
 	return root
 
-tree = ElementTree(cria_carro())
-tree.write("oba.urdf")
 
-#print(dic_links[0].link)
+def escreve_urdf():
+	tree = ElementTree(cria_urdf())
+	tree.write("oba.urdf")
+
+#print(dic_links, dic_joints)
